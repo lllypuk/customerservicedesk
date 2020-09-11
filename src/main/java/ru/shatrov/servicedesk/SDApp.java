73a61e35ea2c036f8c1698a39d6141770ced1070
ashatrov <ashatrov@nas.company>
2020-09-11 23:34:14 +0500

    SpringBootApplication

---
+++ src/main/java/ru/shatrov/servicedesk/SDApp.java
@@ -0,0 +1,12 @@
+package ru.shatrov.servicedesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SDApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SDApp.class, args);
+    }
+}
